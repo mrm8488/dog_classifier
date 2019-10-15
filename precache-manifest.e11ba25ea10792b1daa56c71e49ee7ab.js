a3226c7d475beb611ad8a866dcafd6aea221172e
mrm8488 <mrm8488@gmail.com>
2019-10-15 23:55:33 +0200

    Updates

---
+++ precache-manifest.e11ba25ea10792b1daa56c71e49ee7ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2de05f9eb40c9f872e3d950b98f9eb7",
+    "url": "/dog_classifier/index.html"
+  },
+  {
+    "revision": "169bc05101fb8d0d23a2",
+    "url": "/dog_classifier/static/js/2.f5db8d94.chunk.js"
+  },
+  {
+    "revision": "cab13f262c54047c53a7",
+    "url": "/dog_classifier/static/js/main.faad8c4c.chunk.js"
+  },
+  {
+    "revision": "0389c3966a41b65ba11d",
+    "url": "/dog_classifier/static/js/runtime-main.d7b8b109.js"
+  },
+  {
+    "revision": "e15a286155536bf7a2872a2f0846b550",
+    "url": "/dog_classifier/static/media/dogs-resnet18.e15a2861.onnx"
+  }
+]);
