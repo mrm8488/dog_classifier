3eb65df6a756022f0d84dbf53be0625dc5cbc6fc
mrm8488 <mrm8488@gmail.com>
2019-10-15 23:41:04 +0200

    First commit

---
+++ build/precache-manifest.c91b86205a577f1b81c3a7f4334ef14e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484f11df5e69ae396678753ceda0a249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169bc05101fb8d0d23a2",
+    "url": "/static/js/2.f5db8d94.chunk.js"
+  },
+  {
+    "revision": "dc5102a09a72f18d87e5",
+    "url": "/static/js/main.25c0e53d.chunk.js"
+  },
+  {
+    "revision": "1a357bef4cfa8921602a",
+    "url": "/static/js/runtime-main.d3a6f1ae.js"
+  },
+  {
+    "revision": "e15a286155536bf7a2872a2f0846b550",
+    "url": "/static/media/dogs-resnet18.e15a2861.onnx"
+  }
+]);
